7f6da3899d69423097bcd192273f256d8e20721c
angoudjou <ngoudjou@yahoo.fr>
2024-06-05 10:32:59 -0400

    Add : Person and Identification classes
     A person can have multipe Ids

---
+++ src/PersonneClassLibrairy/PersonneClassLibrairy/Identification.cs
@@ -0,0 +1,10 @@
+﻿namespace PersonneClassLibrairy
+{
+    public class Identification
+    {
+        public string ID { get; set; }
+        public string? Name { get; set; }
+        public DateOnly  ExpireOn { get; set; }
+        public string Note { get; set; }
+    }
+}
---
+++ src/PersonneClassLibrairy/PersonneClassLibrairy/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonneClassLibrairy
+{
+    public  class Person
+    {
+        public string ID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateOnly DOB { get; set; }= default(DateOnly);
+
+        public ICollection<Identification> identification { get; set; }
+    }
+}

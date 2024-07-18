9af831cb894ce6d9ade3d355192d45a131d4ead9
angoudjou <ngoudjou@yahoo.fr>
2024-07-18 16:36:24 -0400

    Update the TenantplugIn version to include all version

--- src/PersonneClassLibrairy/PersonneClassLibrairy/AnotherClass.cs
+++ src/PersonneClassLibrairy/PersonneClassLibrairy/AnotherClass.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TenantPlugIn;
 
 namespace PersonneClassLibrairy;
 
 public class AnotherClass
 {
     public TenantPlugIn.Tenant Tenant { get; set; }
-    
+
+    public AnotherClass(Tenant t)
+    {
+        Tenant = t;
+    }
+
 }
